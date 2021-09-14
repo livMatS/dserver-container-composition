5adcd290b9593ad18d4a6b1bb4a5cb310d630664
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-09-15 01:03:56 +0200

    FIX: grant privileges to s3 testuser

--- compose/minio-init/init.sh
+++ compose/minio-init/init.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+# configure alias for sminio host
 mc config host add s3server http://s3server:9000 $(</run/secrets/access_key) $(</run/secrets/secret_key)
 ret=$?
 if [ $ret -ne 0 ]; then
     exit $ret
 fi
+# create test user
 mc admin user add s3server testuser_access_key testuser_secret_key
 ret=$?
 if [ $ret -ne 0 ]; then
     exit $ret
 fi
+# assign privileges to user
+mc admin policy set s3server readwrite user=testuser_access_key
+ret=$?
+if [ $ret -ne 0 ]; then
+    exit $ret
+fi
+# create bucket
 mc mb s3server/test-bucket
 # non-zero exit code might just mean the bucket exists already
 exit 0
