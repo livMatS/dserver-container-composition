3cc284bb032aec277abf904c21379b5b3577812b
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-09-15 11:41:25 +0200

    MAINT: client image v0.2.1, tests

---
+++ tests/run.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -x
+#
+# This snippet is meant for running tests quickly locally and may be outdated.
+# The proper testing workflow resides within .github/workflows/test.yml
+#
+# Run test script from within repository root via
+#
+#   bash tests/run.sh
+#
+# Docker is required.
+#
+set -euxo pipefail
+docker-compose down --volumes --timeout 30
+
+docker-compose pull
+echo "docker image ls"
+docker image ls
+
+echo "generate certs and keys"
+cd keys
+bash generate.sh
+bash generate_jwt_key.sh
+cd ..
+
+docker-compose -p dtool-lookup-server-container-composition up -d
+
+sleep 60 # TODO: mechanism to wait for containers to be ready
+
+echo "docker container ls --all"
+docker container ls --all
+
+echo "docker volume ls"
+docker volume ls
+
+echo "docker images"
+docker-compose images
+
+echo "dtool lookup server log"
+docker-compose logs dtool_lookup_server
+
+echo "dtool lookup client log"
+docker-compose logs dtool_lookup_client
+
+echo "dtool ls smb://test-share"
+docker-compose run dtool_lookup_client dtool ls smb://test-share
+
+echo "dtool ls s3://test-bucket"
+docker-compose run dtool_lookup_client dtool ls s3://test-bucket
+
+echo "explicitly re-evoke dataset indexing"
+docker-compose exec -T dtool_lookup_server /refresh_index
+
+echo "dtool query '{}'"
+docker-compose run dtool_lookup_client dtool query '{}'
+
+# echo "docker-compose down --volumes"
+# docker-compose down --volumes --timeout 30
