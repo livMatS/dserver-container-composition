eb8ef575e6e21ffba82cf7c1f224c86a2e950c42
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-10-04 18:11:09 +0200

    MAINT: split testing configuration working on simdata

--- tests/run.sh
+++ tests/run.sh
@@ -3,16 +3,20 @@
 # This snippet is meant for running tests quickly locally and may be outdated.
 # The proper testing workflow resides within .github/workflows/test.yml
 #
-# Run test script from within repository root via
+# Run test script from within repository root i.e. via
 #
+#   export DOCKER_COMPOSE_OPTS="-f docker-compose.yml -f docker-compose.external-envs.yml -f docker-compose.alt-ports.yml -f docker-compose.testing.yml"
 #   bash tests/run.sh
 #
 # Docker is required.
 #
 set -euxo pipefail
-docker-compose down --volumes --timeout 30
 
-docker-compose pull
+DOCKER_COMPOSE_OPTS=${DOCKER_COMPOSE_OPTS:-"-f docker-compose.yml -f docker-compose.default-envs.yml -f docker-compose.default-ports.yml -f docker-compose.testing.yml"}
+
+docker-compose ${DOCKER_COMPOSE_OPTS} down --volumes --timeout 30
+
+docker-compose ${DOCKER_COMPOSE_OPTS} pull
 echo "docker image ls"
 docker image ls
 
@@ -22,7 +26,7 @@ bash generate.sh
 bash generate_jwt_key.sh
 cd ..
 
-docker-compose -p dtool-lookup-server-container-composition up -d
+docker-compose ${DOCKER_COMPOSE_OPTS} -p dtool-lookup-server-container-composition up -d
 
 sleep 10 # TODO: mechanism to wait for containers to be ready
 
@@ -33,36 +37,36 @@ echo "docker volume ls"
 docker volume ls
 
 echo "docker images"
-docker-compose images
+docker-compose ${DOCKER_COMPOSE_OPTS} images
 
 echo "dtool lookup server log"
-docker-compose logs dtool_lookup_server
+docker-compose ${DOCKER_COMPOSE_OPTS} logs dtool_lookup_server
 
 echo "dtool lookup client log"
-docker-compose logs dtool_lookup_client
+docker-compose ${DOCKER_COMPOSE_OPTS} logs dtool_lookup_client
 
 # create dtool directory on samba share
 echo "smbclient -U guest -c "mkdir dtool" -N -W WORKGROUP //sambaserver/sambashare"
-docker-compose run --entrypoint smbclient dtool_lookup_client -U guest -c "mkdir dtool" -N -W WORKGROUP //sambaserver/sambashare
+docker-compose ${DOCKER_COMPOSE_OPTS} run --entrypoint smbclient dtool_lookup_client -U guest -c "mkdir dtool" -N -W WORKGROUP //sambaserver/sambashare
 
 # place test datasets on storage infrastructure
 echo "dtool cp tests/dtool/simple_test_dataset smb://test-share"
-docker-compose run -v $(pwd)/tests:/tests dtool_lookup_client cp /tests/dtool/simple_test_dataset smb://test-share
+docker-compose ${DOCKER_COMPOSE_OPTS} run -v $(pwd)/tests:/tests dtool_lookup_client cp /tests/dtool/simple_test_dataset smb://test-share
 
 echo "dtool cp tests/dtool/simple_test_dataset s3://test-bucket"
-docker-compose run -v $(pwd)/tests:/tests dtool_lookup_client cp /tests/dtool/simple_test_dataset s3://test-bucket
+docker-compose ${DOCKER_COMPOSE_OPTS} run -v $(pwd)/tests:/tests dtool_lookup_client cp /tests/dtool/simple_test_dataset s3://test-bucket
 
 echo "dtool ls smb://test-share"
-docker-compose run dtool_lookup_client ls smb://test-share
+docker-compose ${DOCKER_COMPOSE_OPTS} run dtool_lookup_client ls smb://test-share
 
 echo "dtool ls s3://test-bucket"
-docker-compose run dtool_lookup_client ls s3://test-bucket
+docker-compose ${DOCKER_COMPOSE_OPTS} run dtool_lookup_client ls s3://test-bucket
 
 echo "explicitly re-evoke dataset indexing"
-docker-compose exec -T dtool_lookup_server /refresh_index
+docker-compose ${DOCKER_COMPOSE_OPTS} exec -T dtool_lookup_server /refresh_index
 
 echo "dtool query '{}'"
-docker-compose run dtool_lookup_client query '{}'
+docker-compose ${DOCKER_COMPOSE_OPTS} run dtool_lookup_client query '{}'
 
 # echo "docker-compose down --volumes"
 # docker-compose down --volumes --timeout 30
