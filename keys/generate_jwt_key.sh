011323a012c1542bdcd8dcf344f111fd64634d56
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-11-03 09:40:57 +0100

    MAINT: missing directory creation

--- keys/generate_jwt_key.sh
+++ keys/generate_jwt_key.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+mkdir -p dtool_lookup_server
+mkdir -p token_generator_ldap
+
 openssl genrsa -out dtool_lookup_server/jwt_key 2048
 openssl rsa -in dtool_lookup_server/jwt_key -pubout -outform PEM -out dtool_lookup_server/jwt_key.pub
 
-mkdir -p dtool_lookup_server
 cp dtool_lookup_server/jwt_key token_generator_ldap/jwt_key
 cp dtool_lookup_server/jwt_key.pub token_generator_ldap/jwt_key.pub
