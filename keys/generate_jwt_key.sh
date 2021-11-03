43f1b635bb55c3e606f88a3e06ba04b5ac38397d
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2021-11-03 09:29:14 +0100

    MAINT: standalone jwt key generation

--- keys/generate_jwt_key.sh
+++ keys/generate_jwt_key.sh
@@ -3,5 +3,6 @@
 openssl genrsa -out dtool_lookup_server/jwt_key 2048
 openssl rsa -in dtool_lookup_server/jwt_key -pubout -outform PEM -out dtool_lookup_server/jwt_key.pub
 
+mkdir -p dtool_lookup_server
 cp dtool_lookup_server/jwt_key token_generator_ldap/jwt_key
 cp dtool_lookup_server/jwt_key.pub token_generator_ldap/jwt_key.pub
