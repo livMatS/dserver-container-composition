b6c5efe038e47a3dac0e093e495e2a72d5ccfaf8
Cloud User <centos@dtool-lookup-server-public.novalocal>
2022-02-09 10:30:43 +0000

    MAINT: split tests/run.sh into tests/run.sh and tests/init.sh

--- tests/run.sh
+++ tests/run.sh
@@ -12,7 +12,13 @@
 #
 set -euxo pipefail
 
-DOCKER_COMPOSE_OPTS=${DOCKER_COMPOSE_OPTS:-"-f docker-compose.yml -f docker-compose.default-envs.yml -f docker-compose.default-ports.yml -f docker-compose.testing.yml"}
+DOCKER_COMPOSE_OPTS=${DOCKER_COMPOSE_OPTS:-"-p dtool-lookup- server \
+                                           -f docker-compose.yml \
+                                           -f docker-compose.versions.yml \
+                                           -f docker-compose.default-envs.yml \
+                                           -f docker-compose.default-ports.yml \
+                                           -f docker-compose.testing.yml \
+                                           -f docker-compose.testing.versions.yml"}
 
 docker-compose ${DOCKER_COMPOSE_OPTS} down --volumes --timeout 30
 
@@ -42,28 +48,4 @@ docker-compose ${DOCKER_COMPOSE_OPTS} logs dtool_lookup_server
 echo "dtool lookup client log"
 docker-compose ${DOCKER_COMPOSE_OPTS} logs dtool_lookup_client
 
-# create dtool directory on samba share
-echo "smbclient -U guest -c "mkdir dtool" -N -W WORKGROUP //sambaserver/sambashare"
-docker-compose ${DOCKER_COMPOSE_OPTS} run --entrypoint smbclient dtool_lookup_client -U guest -c "mkdir dtool" -N -W WORKGROUP //sambaserver/sambashare
-
-# place test datasets on storage infrastructure
-echo "dtool cp tests/dtool/simple_test_dataset smb://test-share"
-docker-compose ${DOCKER_COMPOSE_OPTS} run -v $(pwd)/tests:/tests dtool_lookup_client cp /tests/dtool/simple_test_dataset smb://test-share
-
-echo "dtool cp tests/dtool/simple_test_dataset s3://test-bucket"
-docker-compose ${DOCKER_COMPOSE_OPTS} run -v $(pwd)/tests:/tests dtool_lookup_client cp /tests/dtool/simple_test_dataset s3://test-bucket
-
-echo "dtool ls smb://test-share"
-docker-compose ${DOCKER_COMPOSE_OPTS} run dtool_lookup_client ls smb://test-share
-
-echo "dtool ls s3://test-bucket"
-docker-compose ${DOCKER_COMPOSE_OPTS} run dtool_lookup_client ls s3://test-bucket
-
-echo "explicitly re-evoke dataset indexing"
-docker-compose ${DOCKER_COMPOSE_OPTS} exec -T dtool_lookup_server /refresh_index
-
-echo "dtool query '{}'"
-docker-compose ${DOCKER_COMPOSE_OPTS} run dtool_lookup_client query '{}'
-
-# echo "docker-compose down --volumes"
-# docker-compose down --volumes --timeout 30
+bash tests/init.sh
